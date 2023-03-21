5260d6629a5336848d2df66eb417a307b8570a9c
zephkelly <masterevank@gmail.com>
2023-03-22 01:42:20 +1100

    Modularise createBranch and install Jest

--- api/createBranch.ts
+++ api/createBranch.ts
@@ -1,38 +1,48 @@
-import { mongoose } from '@/plugins/mongo';
-import { pool } from '@/plugins/postgres';
+import { mongoose } from './../plugins/mongo';
+import { pool } from './../plugins/postgres';
 require('dotenv').config();
 
-async function createBranch(name: string, description: string) {
+export async function createBranch(name: string, description: string) {
   try {
-    //Make sure that the branches table exists
-    await pool.query(
-      'CREATE TABLE IF NOT EXISTS branches (id TEXT PRIMARY KEY, name TEXT NOT NULL, description TEXT NOT NULL, created_at TIMESTAMP NOT NULL DEFAULT NOW(), posts_collection TEXT NOT NULL)'
-    );
-
-    // Insert the new branch into the branches table
-    const newBranchMeta = await pool.query(
-      'INSERT INTO branches (name, description) VALUES ($1, $2) RETURNING *',
-      [name, description]
-    );
-
-    // Create a new collection in mongoDb for the branch using the name of the new database
-    const newBranchCollection = await mongoose.createConnection(
-      `${process.env.MONGO_CONNECTION + name}`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-
-    // Store the name of the new collection in the 'posts_collection' field of the branch in PostgreSQL
-    await pool.query(
-      'UPDATE branches SET posts_collection = $1 WHERE id = $2',
-      [newBranchCollection.id, name]
-    );
+    await doesTableExist(name);
+    await createBranchMeta(name, description);
+    await createBranchCollection(name);
+    await insertReferenceToBranchCollection(name, name);
   }
   catch (err)
   {
     console.error(err);
     throw err;
   }
+}
+
+async function doesTableExist(tableName: string) {
+  return await pool.query(
+    'SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = $1)',
+    [tableName]
+  );
+}
+
+async function createBranchMeta(name: string, description: string) {
+  return await pool.query(
+    'INSERT INTO branches (name, description) VALUES ($1, $2) RETURNING *',
+    [name, description]
+  );
+}
+
+async function createBranchCollection(name: string) {
+  return await mongoose.createConnection(
+      `${process.env.MONGO_CONNECTION + name}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+  );
+}
+
+async function insertReferenceToBranchCollection(name: string, collection: string) {
+  return await pool.query(
+    'UPDATE branches SET posts_collection = $1 WHERE id = $2',
+    [collection, name]
+  );
 }
