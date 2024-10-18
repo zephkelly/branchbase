767ba2397d56fccd76c1b01fd36272af74883997
zephkelly <masterevank@gmail.com>
2024-10-18 13:51:12 +1100

    Add tests for input sanitisation

--- utils/inputSanitisation.ts
+++ utils/inputSanitisation.ts
@@ -1,4 +1,4 @@
-const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 
 /**
  * Validates an email address.
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,23 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
+import vue from '@vitejs/plugin-vue'
+import { ModuleResolutionKind } from 'typescript'
+import { fileURLToPath } from 'url'
 
 export default defineVitestConfig({
-
+    plugins: [vue()],
+    test: {
+        exclude: ['**/node_modules/**'],
+        globals: true,
+        environment: 'happy-dom',
+        environmentOptions: {
+            nuxt: {
+              rootDir: fileURLToPath(new URL('.', import.meta.url)),
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./', import.meta.url)),
+        },
+    },
 })
